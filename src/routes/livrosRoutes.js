19c1bd12e909a966c697b1eb7c185fd7b3c2eee2
marksv21 <marvmms@gmail.com>
2023-11-10 08:47:08 -0300

    add route de busca

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,10 +5,10 @@ import LivroController from "../controllers/livroController";
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
 routes.get("/livros/:id", LivroController.listarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivro);
 routes.delete("/livros/:id", LivroController.excluirLivro);
-
 
 export default routes;
