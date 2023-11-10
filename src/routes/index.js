f2b951173411e3b4bd96a92a91ea13597367805f
marksv21 <marvmms@gmail.com>
2023-11-10 08:46:35 -0300

    create route autor

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import  express  from "express";
 import livros from "./livrosRoutes.js";
-
+import autores from "./autoresRoutes.js";
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send("curso de node.js");
   
-    app.use(express.json(), livros);
+    app.use(express.json(), livros, autores);
   });
   
 };
