f6ca1582ba3d3a116de2fb5f46e10ea0a34f8af6
marksv21 <marvmms@gmail.com>
2023-12-28 23:11:46 -0300

    fix: config project

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,8 @@
-import mongoose, {mongo} from "mongoose"
-
+import mongoose, { mongo } from "mongoose";
 
 async function dbConnect() {
   mongoose.connect(process.env.DB_CONNECTION_STRING);
   return mongoose.connection;
-}
-
+};
 
 export default dbConnect;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import LivroController from "../controllers/livroController";
+import LivroController from "../controllers/livroController.js";
 
 
 const routes = express.Router();
