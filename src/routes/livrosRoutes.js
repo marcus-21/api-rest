6905bc5adb4b85aa20dfb9f00a568303976a568b
marksv21 <marvmms@gmail.com>
2024-01-08 00:40:17 -0300

    add pagination in routes

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import LivroController from "../controllers/livroController.js";
+import paginar from "../../src/middlewares/paginar.js";
 
 
 const routes = express.Router();
 
-routes.get("/livros", LivroController.listarLivros);
-routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
+routes.get("/livros", LivroController.listarLivros, paginar);
+routes.get("/livros/busca", LivroController.listarLivroPorFiltro, paginar);
 routes.get("/livros/:id", LivroController.listarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivro);
